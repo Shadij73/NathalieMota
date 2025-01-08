4ce2689c2ed2e12852b435aa37d92c99425157fe
Shadi <shadij73@hotmail.com>
2025-01-08 09:29:18 +0100

    Update: Version 2.0 - Added contact modal

--- footer.php
+++ footer.php
@@ -1,3 +1,6 @@
+<?php get_template_part('templates_part/contact-modal'); ?>
+<script src="<?php echo get_template_directory_uri(); ?>/js/scripts.js"></script>
+
 <footer>
     <p>&copy; <?php echo date('Y'); ?> Tous droits réservés.</p>
 </footer>
