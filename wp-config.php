c101098a8e8c8c0743aea3273b334f70cf64a522
Shadi <shadij73@hotmail.com>
2025-01-10 02:53:26 +0100

    Update: Version 4.0 Contact modal styling to match design

---
+++ wp-config.php
@@ -0,0 +1,103 @@
+<?php
+define( 'WP_CACHE', true );
+/**
+ * The base configuration for WordPress
+ *
+ * The wp-config.php creation script uses this file during the installation.
+ * You don't have to use the website, you can copy this file to "wp-config.php"
+ * and fill in the values.
+ *
+ * This file contains the following configurations:
+ *
+ * * Database settings
+ * * Secret keys
+ * * Database table prefix
+ * * ABSPATH
+ *
+ * @link https://developer.wordpress.org/advanced-administration/wordpress/wp-config/
+ *
+ * @package WordPress
+ */
+
+// ** Database settings - You can get this info from your web host ** //
+/** The name of the database for WordPress */
+define( 'DB_NAME', 'NathalieMota' );
+
+/** Database username */
+define( 'DB_USER', 'root' );
+
+/** Database password */
+define( 'DB_PASSWORD', '' );
+
+/** Database hostname */
+define( 'DB_HOST', 'localhost' );
+
+/** Database charset to use in creating database tables. */
+define( 'DB_CHARSET', 'utf8mb4' );
+
+/** The database collate type. Don't change this if in doubt. */
+define( 'DB_COLLATE', '' );
+
+/**#@+
+ * Authentication unique keys and salts.
+ *
+ * Change these to different unique phrases! You can generate these using
+ * the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}.
+ *
+ * You can change these at any point in time to invalidate all existing cookies.
+ * This will force all users to have to log in again.
+ *
+ * @since 2.6.0
+ */
+define( 'AUTH_KEY',         ']!i1zRaB6%LuIx.D=8ZRZz=O(^;Yp:>]Xan[85q<tyxzFo.#Y|Z Re())~U#RUEr' );
+define( 'SECURE_AUTH_KEY',  'U;x!]-;t}KtC)~Qkf^PmD,`M{5:>bZsEElCE)rZ,R2?h`Azo=CaMN&u8}!xtY0Tf' );
+define( 'LOGGED_IN_KEY',    '_^UBlOECI^l &8}aga,^ya9n7N/<tJW8uH))Tx#bb7/9`W &Y3O$<$.$K7mE/YLb' );
+define( 'NONCE_KEY',        'Nx&z2)S)`y9}D#]VD{}uBHNmnr8JGJ5lYNhidK;PuAU=yRTItD1?%K54ubeGTDAF' );
+define( 'AUTH_SALT',        'hkDHA`U!qX$ep]j2.f5/k?,+9eCdi|R[&?w.GYM_2&lCIh Ox;xwSr$=nYbztLU,' );
+define( 'SECURE_AUTH_SALT', '&NkoRrk~vTU2QzbS+)a>-W/xu&ylU*R):Xp|xNAf/r6wpn{8FQ^@4]tw5 [*5N1A' );
+define( 'LOGGED_IN_SALT',   'o!#>0NA=tpK,<lpv$[Vucrvup_soDOP{I00[?JWue>g!-Z~if~?MSU<ZQq51bv:=' );
+define( 'NONCE_SALT',       'IfRYDf[?L#$$F,%66XZJ>{1gnffr*Y9TvaP=dASP.d1@S5O2u-..nh>^qG2B!,Pv' );
+
+/**#@-*/
+
+/**
+ * WordPress database table prefix.
+ *
+ * You can have multiple installations in one database if you give each
+ * a unique prefix. Only numbers, letters, and underscores please!
+ *
+ * At the installation time, database tables are created with the specified prefix.
+ * Changing this value after WordPress is installed will make your site think
+ * it has not been installed.
+ *
+ * @link https://developer.wordpress.org/advanced-administration/wordpress/wp-config/#table-prefix
+ */
+$table_prefix = 'wp_';
+
+/**
+ * For developers: WordPress debugging mode.
+ *
+ * Change this to true to enable the display of notices during development.
+ * It is strongly recommended that plugin and theme developers use WP_DEBUG
+ * in their development environments.
+ *
+ * For information on other constants that can be used for debugging,
+ * visit the documentation.
+ *
+ * @link https://developer.wordpress.org/advanced-administration/debug/debug-wordpress/
+ */
+define( 'WP_DEBUG', false );
+
+/* Add any custom values between this line and the "stop editing" line. */
+
+
+
+/* That's all, stop editing! Happy publishing. */
+
+/** Absolute path to the WordPress directory. */
+if ( ! defined( 'ABSPATH' ) ) {
+	define( 'ABSPATH', __DIR__ . '/' );
+}
+
+/** Sets up WordPress vars and included files. */
+require_once ABSPATH . 'wp-settings.php';
---
+++ wp-content/themes/NathalieMota - Copy/functions.php
@@ -0,0 +1,36 @@
+<?php
+function custom_enqueue_assets() {
+    // Preconnect to Google Fonts
+    echo '<link rel="preconnect" href="https://fonts.googleapis.com">' . "\n";
+    echo '<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>' . "\n";
+
+    // Enqueue Google Fonts
+    wp_enqueue_style('custom-google-fonts', 'https://fonts.googleapis.com/css2?family=Space+Mono:ital,wght@0,400;0,700;1,400;1,700&display=swap', false);
+}
+add_action('wp_head', 'custom_enqueue_assets', 5); // Loads in <head> for better performance
+
+// Enqueue theme scripts and styles
+function theme_enqueue_scripts() {
+    wp_enqueue_style('theme-style', get_stylesheet_uri());
+    wp_enqueue_script('theme-scripts', get_template_directory_uri() . '/js/scripts.js', array('jquery'), null, true);
+}
+add_action('wp_enqueue_scripts', 'theme_enqueue_scripts');
+
+// Register navigation menu
+function register_theme_menus() {
+    register_nav_menus(array(
+        'main-menu' => __('Main Menu', 'nathaliemota'),
+    ));
+}
+add_action('after_setup_theme', 'register_theme_menus');
+
+// Add theme support
+function theme_setup() {
+    add_theme_support('title-tag');
+    add_theme_support('post-thumbnails');
+}
+add_action('after_setup_theme', 'theme_setup');
+
+?>
+
+
