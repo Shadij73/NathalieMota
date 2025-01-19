d1a2264073faca3e67bdcb106a82331fff5fa635
Shadi <shadij73@hotmail.com>
2025-01-19 12:14:23 +0100

    V05

---
+++ wp-content/themes/NathalieMota/templates_part/modal.php
@@ -0,0 +1,10 @@
+<!-- Modal for Contact -->
+<div id="myModal" class="modal">
+  <!-- Modal Content -->
+  <div class="modal-content">
+        <!-- Modal Header Image -->
+        <img src="<?php echo get_template_directory_uri(); ?>/img_logo/Contact header.png" alt="Image Modal">
+        <!-- Contact Form 7 Shortcode -->
+        <?php echo do_shortcode('[contact-form-7 id="2cc0601" title="Modal Contact 1"]'); ?>
+  </div>
+</div>
